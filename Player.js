01088927e2d41de0daac624195acd78b0da20bdb
Rankangi <matiou.bann@gmail.com>
2019-05-21 14:26:36 +0200

    Transfère des fonctions dans un fichier mylib.

--- Player.js
+++ Player.js
@@ -38,27 +38,3 @@ class Player {
     }
   }
 }
-
-function createQ() {
-  var Q = [];
-  var L;
-
-  for (var j = 0 ; j<row ; j++){ //
-    L = [];
-
-    for (var i = 0 ; i<col ; i++){ //
-      L.push([0,0,0,0]);
-    }
-    Q.push(L);
-  }
-
-  return Q;
-}
-
-function randint(max) {
-  return Math.floor(Math.random()*Math.floor(max));
-}
-
-function argMax(array) {
-  return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
-}
